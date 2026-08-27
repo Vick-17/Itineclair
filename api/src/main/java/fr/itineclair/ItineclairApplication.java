cc5b93f801ad3992d431c9bd77df32c705950497
Vick-17 <vega.victor.0712@gmail.com>
2026-08-27 11:03:27 +0200

    chore: modernize Itineclair project foundation

---
+++ api/src/main/java/fr/itineclair/ItineclairApplication.java
@@ -0,0 +1,12 @@
+package fr.itineclair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineclairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItineclairApplication.class, args);
+    }
+}
